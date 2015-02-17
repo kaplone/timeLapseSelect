ba9ca1ec0ef796383c9d57d7b12f231fb378a3ef
kaplone <dvd.brtrnd@gmail.com>
2015-02-17 18:16:17 +0100

    nombreuses corrections

--- timelapseSelect/src/timeSelect/MainTS.java
+++ timelapseSelect/src/timeSelect/MainTS.java
@@ -21,6 +21,10 @@ public class MainTS extends Application {
 	public static void main(String[] args) {
 		launch(args);
 		
+		// selection :
 		// mencoder mf://@liste_select.txt -mf w=1000:h=750:fps=25:type=jpg -ovc copy -oac copy -o sortie9.mov
+		//
+		// complet pour client :
+		// mencoder mf://../soletanche/*.jpg -mf fps=25:type=jpg -ovc x264 subq=5:partitions=all:8x8dct:frameref=2:bframes=3:b_pyramid=normal:weight_b -o ../soletanche.mp4
 	}
 }
